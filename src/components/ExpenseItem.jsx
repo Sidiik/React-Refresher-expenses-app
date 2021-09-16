f5cab0dc1db8ffb3e1125933e04994c6c9ec3683
Sadiq O <sadiqomar890@gmail.com>
2021-09-16 13:43:28 +0300

    init

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,16 @@
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div className="e-date">
+        <h2>Mar 23rd 2020</h2>
+      </div>
+      <div className="e-description">
+        <h2>Car Insurance</h2>
+      </div>
+      <div className="e-price">
+        <h2> $297.67</h2>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
